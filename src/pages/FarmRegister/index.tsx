0e0f59ed8fd2fb62c24536232ae9edec5bcfb404
renatogual <renatogual@hotmail.com>
2024-09-04 09:42:57 -0300

    feat: add cpf and cnpj input validation

--- src/pages/FarmRegister/index.tsx
+++ src/pages/FarmRegister/index.tsx
@@ -7,12 +7,17 @@ import {
     Select,
     Row,
     Col,
+    Space,
+    Typography
 } from 'antd';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import { Farm } from '../../types';
 import { useFarm } from '../../contexts/FarmContext';
-import { useEffect } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { isCnpjValid, isCpfValid } from '../../utils';
+
+const { Title } = Typography
 
 const cropsOptions = [
     {
@@ -43,12 +48,53 @@ const FarmRegister = () => {
     const { id } = useParams()
     const { farms, addFarm, editFarm } = useFarm()
 
+    const [inputValue, setInputValue] = useState('');
+
+    const pageTitle = id ? 'Editar fazenda:' : 'Adicionar fazenda:'
+    const requiredMessage = 'Campo obrigatório'
+
     function handleSubmit(fieldsValue: Farm) {
         const action = id ? editFarm : addFarm
         const updatedValues = id ? { ...fieldsValue, id } : fieldsValue
         action(updatedValues)
         navigate('/farms')
     }
+
+    function validateCPFOrCNPJ(_: any, value: string) {
+        const cleanedValue = value.replace(/\D/g, '');
+        if (!value) {
+            return Promise.reject(new Error('Campo obrigatório'));
+        } else if (cleanedValue.length <= 11 && !isCpfValid(value)) {
+            return Promise.reject(new Error('CPF inválido'));
+        } else if (cleanedValue.length > 11 && !isCnpjValid(value)) {
+            return Promise.reject(new Error('CNPJ inválido'));
+        }
+        return Promise.resolve();
+    };
+
+    const handleCpfOrCnpjChange = (event: ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value.replace(/\D/g, '');
+
+        if (value.length > 14) {
+            value = value.slice(0, 14);
+        }
+
+        if (value.length <= 11) {
+            // Apply CPF mask
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+        } else {
+            // Apply CNPJ mask
+            value = value.replace(/^(\d{2})(\d)/, '$1.$2');
+            value = value.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+            value = value.replace(/\.(\d{3})(\d)/, '.$1/$2');
+            value = value.replace(/(\d{4})(\d{1,2})$/, '$1-$2');
+        }
+
+        setInputValue(value);
+        form.setFieldsValue({ cpfCnpj: value });
+    };
 
     useEffect(() => {
         if (!id) return
@@ -58,102 +104,108 @@ const FarmRegister = () => {
     }, [id])
 
     return (
-        <Form
-            form={form}
-            variant='outlined'
-            layout='vertical'
-            size='large'
-            onFinish={handleSubmit}
-        >
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item label="CPF/CPNJ" name="cpfCnpj" rules={[{ required: true, message: 'Please input!' }]}>
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        label="Área total em hectares da fazenda"
-                        name="totalArea"
-                        rules={[{ required: true, message: 'Please input!' }]}
-                    >
-                        <InputNumber style={{ width: '100%' }} />
-                    </Form.Item>
-                </Col>
-            </Row>
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item label="Produtor" name="ownerName" rules={[{ required: true, message: 'Please input!' }]}>
-                        <Input />
-                    </Form.Item>
+        <Space direction='vertical' style={{ width: '100%' }}>
+            <Title level={4}>{pageTitle}</Title>
+            <Form
+                form={form}
+                variant='outlined'
+                layout='vertical'
+                size='large'
+                onFinish={handleSubmit}
+            >
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Form.Item label="CPF ou CNPJ" name="cpfCnpj" rules={[{ validator: validateCPFOrCNPJ }]}>
+                            <Input
+                                value={inputValue}
+                                onChange={handleCpfOrCnpjChange}
+                            />
+                        </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item
+                            label="Área total em hectares da fazenda"
+                            name="totalArea"
+                            rules={[{ required: true, message: requiredMessage }]}
+                        >
+                            <InputNumber style={{ width: '100%' }} />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Form.Item label="Produtor" name="ownerName" rules={[{ required: true, message: requiredMessage }]}>
+                            <Input />
+                        </Form.Item>
 
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        label="Área agricultável em hectares"
-                        name="arableArea"
-                        rules={[{ required: true, message: 'Please input!' }]}
-                    >
-                        <InputNumber style={{ width: '100%' }} />
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item
+                            label="Área agricultável em hectares"
+                            name="arableArea"
+                            rules={[{ required: true, message: requiredMessage }]}
+                        >
+                            <InputNumber style={{ width: '100%' }} />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Form.Item label="Fazenda" name="farmName" rules={[{ required: true, message: requiredMessage }]}>
+                            <Input />
+                        </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item
+                            label="Área de vegetação em hectares"
+                            name="vegetationArea"
+                            rules={[{ required: true, message: requiredMessage }]}
+                        >
+                            <InputNumber style={{ width: '100%' }} />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Form.Item label="Cidade" name="city" rules={[{ required: true, message: requiredMessage }]}>
+                            <Input />
+                        </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item
+                            label="Culturas plantadas"
+                            name="crops"
+                            rules={[{ required: true, message: requiredMessage }]}
+                        >
+                            <Select
+                                placeholder="Selecione a cultura plantada"
+                                allowClear
+                                mode="multiple"
+                                options={cropsOptions}
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={24}>
+                    <Col span={12}>
+                        <Form.Item label="Estado" name="state" rules={[{ required: true, message: requiredMessage }]}>
+                            <Input />
+                        </Form.Item>
+                    </Col>
+                    <Col span={12} />
+                </Row>
+                <Row justify='end' style={{ gap: 8 }}>
+                    <Link to="/farms">
+                        <Button>Cancelar</Button>
+                    </Link>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Salvar
+                        </Button>
                     </Form.Item>
-                </Col>
-            </Row>
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item label="Fazenda" name="farmName" rules={[{ required: true, message: 'Please input!' }]}>
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        label="Área de vegetação em hectares"
-                        name="vegetationArea"
-                        rules={[{ required: true, message: 'Please input!' }]}
-                    >
-                        <InputNumber style={{ width: '100%' }} />
-                    </Form.Item>
-                </Col>
-            </Row>
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item label="Cidade" name="city" rules={[{ required: true, message: 'Please input!' }]}>
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12}>
-                    <Form.Item
-                        label="Culturas plantadas"
-                        name="crops"
-                        rules={[{ required: true, message: 'Please input!' }]}
-                    >
-                        <Select
-                            placeholder="Selecione a cultura plantada"
-                            allowClear
-                            mode="multiple"
-                            options={cropsOptions}
-                        />
-                    </Form.Item>
-                </Col>
-            </Row>
-            <Row gutter={24}>
-                <Col span={12}>
-                    <Form.Item label="Estado" name="state" rules={[{ required: true, message: 'Please input!' }]}>
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col span={12} />
-            </Row>
-            <Row justify='end' style={{ gap: 8 }}>
-                <Link to="/farms">
-                    <Button>Cancelar</Button>
-                </Link>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                        Salvar
-                    </Button>
-                </Form.Item>
-            </Row>
-        </Form>
+                </Row>
+            </Form>
+        </Space>
     );
 };
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,61 @@ export const saveToLocalStorage = (key: string, data: unknown) => {
     const jsonData = JSON.stringify(data);
     localStorage.setItem(key, jsonData);
 }
+
+export const isCpfValid = (cpf: string) => {
+    cpf = cpf.replace(/[^\d]+/g, '');
+
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+        sum += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    let checkDigit = 11 - (sum % 11);
+    if (checkDigit === 10 || checkDigit === 11) checkDigit = 0;
+    if (checkDigit !== parseInt(cpf.charAt(9))) return false;
+
+    sum = 0;
+    for (let i = 0; i < 10; i++) {
+        sum += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    checkDigit = 11 - (sum % 11);
+    if (checkDigit === 10 || checkDigit === 11) checkDigit = 0;
+
+    return checkDigit === parseInt(cpf.charAt(10));
+};
+
+export const isCnpjValid = (cnpj: string) => {
+    cnpj = cnpj.replace(/[^\d]+/g, '');
+
+    if (cnpj.length !== 14) return false;
+
+    let length = cnpj.length - 2;
+    let numbers = cnpj.substring(0, length);
+    const digits = cnpj.substring(length);
+    let sum = 0;
+    let pos = length - 7;
+
+    for (let i = length; i >= 1; i--) {
+        sum += parseInt(numbers.charAt(length - i)) * pos--;
+        if (pos < 2) pos = 9;
+    }
+
+    let checkDigit = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+    if (checkDigit !== parseInt(digits.charAt(0))) return false;
+
+    length += 1;
+    numbers = cnpj.substring(0, length);
+    sum = 0;
+    pos = length - 7;
+
+    for (let i = length; i >= 1; i--) {
+        sum += parseInt(numbers.charAt(length - i)) * pos--;
+        if (pos < 2) pos = 9;
+    }
+
+    checkDigit = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+    return checkDigit === parseInt(digits.charAt(1));
+};
+
